4b1ed760beda4d03999ee1aba837d8a927359d90
kevinchang <kevinjchang98@gmail.com>
2022-03-12 23:32:55 -0800

    change number of datapoints in slice

---
+++ fcc_d3_nextjs/hooks/useData.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { csv } from 'd3';
+
+export const useData = (csvUrl: string, numElements: number) => {
+    const [data, setData] = useState<any | undefined>(null);
+
+    useEffect(() => {
+        let row: any = null;
+        if (
+            (d: any) => {
+                d['2020'];
+            }
+        ) {
+            row = (d: any) => {
+                d.Population = +d['2020'];
+                return d;
+            };
+        }
+
+        csv(csvUrl, row).then((data) => {
+            setData(data.slice(0, numElements));
+        });
+    }, []);
+
+    return data;
+};
--- fcc_d3_nextjs/pages/colorPie.tsx
+++ fcc_d3_nextjs/pages/colorPie.tsx
@@ -2,18 +2,13 @@ import { useEffect, useState } from 'react';
 import { csv } from 'd3';
 import { NextPage } from 'next';
 import ColorPieChart from '../components/ColorPieChart';
+import { useData } from '../hooks/useData';
 
 const colorPie: NextPage = () => {
     const csvUrl =
         'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/acd2b8cecfe51c520622fbaf407ee88b8796bfc6/cssNamedColors.csv';
 
-    const [data, setData] = useState<any | undefined>(null);
-
-    useEffect(() => {
-        csv(csvUrl).then((data) => {
-            setData(data);
-        });
-    }, []);
+    const data = useData(csvUrl, 10000);
 
     return <div>{!data ? 'Loading' : <ColorPieChart data={data} />}</div>;
 };
